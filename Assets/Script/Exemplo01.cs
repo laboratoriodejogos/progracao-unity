ba8d584845107ca8014fc606c1f7a71ec65ec717
Daniel <daniel.fb.jr@gmail.com>
2020-03-12 09:51:01 -0300

    Primeira versão

---
+++ Assets/Script/Exemplo01.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exemplo01 : MonoBehaviour
+{
+    /*
+        Inteiro     |   int 
+        Real        |   float
+        Caracteres  |   string
+        Lógico      |   bool
+    */
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int valor2, tentativas = 10;
+        float peso, n1, n2;
+        string nome_Personagem, valor1;
+        bool vida;
+
+        //tentativas = 10;
+        peso = 98.5f;
+        nome_Personagem = "Joao";
+        vida = true;
+        valor1 = "9";
+
+        peso = tentativas;
+        tentativas = (int)peso;
+        print("Tentativas = " + tentativas);
+        print("Peso = " + peso);
+
+        tentativas = int.Parse(valor1);
+        tentativas = tentativas + 10;
+        print("Soma de Tentativas = " + tentativas);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
